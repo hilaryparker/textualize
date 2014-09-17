03bacd9883691042766efd83c8c7bbfef1058fef
Hilary <hilary@etsy.com>
2014-09-16 23:07:12 -0400

    initial build of package

---
+++ R/summary_text.R
@@ -0,0 +1,23 @@
+#' Summary Text
+#'
+#' This function creates summary paragraphs for htest (hypothesis test) objects.
+#' @param test htest object that you want to summarize
+#' @export
+#' @examples
+#' summary_text(prop.test(x = 500,n = 1005))
+summary_text <- function(test){
+  stmt <- NULL
+  
+  stmt <- paste0(
+    "This was a ",
+    test$method,
+    " testing against the null hypothesis that the true population proportion is equal to ",
+    test$null.value,
+    ". The number of observations is ",
+    test$data.name
+  )
+  
+  return(stmt)
+  
+  # figure out how to make [1] go away.
+}
