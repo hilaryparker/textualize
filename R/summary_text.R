18cc1c9eee75ab89785f0c060c4e69feb891da07
Hilary <hilary@etsy.com>
2014-09-17 21:45:10 -0400

    added p-value chunk

--- R/summary_text.R
+++ R/summary_text.R
@@ -14,7 +14,20 @@ summary_text <- function(test){
     " testing against the null hypothesis that the true population proportion is equal to ",
     test$null.value,
     ". The number of observations is ",
-    test$data.name
+    test$data.name,
+    '.
+    
+    The p-value for this test is ',
+    test$p.value,
+    '. This, formally, is defined as the probability of observing a sample proportion that is as or more extreme than the observed sample proportion, assuming that the null hypothesis is true. In this case, this is the probability of observing a sample proportion that is greater than ',
+    test$null.value + abs(test$estimate - test$null.value),
+    ' or less than ',
+    test$null.value - abs(test$estimate - test$null.value), 
+    ' asumming the null hypothesis (that the population proportion is ',
+    test$null.value,
+    ') were really true. A smaller p-value can be used to reject the null hypothesis, and conclude that the data are not consistent with a true population proportion of ', 
+    test$null.value,
+    '. A common cutoff convention is a p-value of less than 0.05.'
   )
   
   return(stmt)
---
+++ eda.R
@@ -0,0 +1,13 @@
+tmp <- prop.test(x=500, n=1008)
+
+tmp$statistic  
+tmp$parameter	
+tmp$p.value	
+tmp$conf.int	
+tmp$estimate	
+tmp$null.value	
+tmp$alternative	
+tmp$method	
+tmp$data.name	
+
+tmp$null.value - abs(tmp$estimate - tmp$null.value)
