a429c4479f21323a39d7e3a1415de956e5e0e177
Hilary <hilary@etsy.com>
2014-09-21 20:46:53 -0400

    started two-sample summary text

--- R/summary_text.R
+++ R/summary_text.R
@@ -7,50 +7,122 @@
 #' summary_text(prop.test(x = 500,n = 1005))
 summary_text <- function(test, alpha=0.05){
   stmt <- NULL
+  onesamp <- TRUE  
+  if (length(test$estimate) == 2) onesamp <- FALSE
 
-  stmt <- paste0(
-    "This was a one-sample proportion test of the null hypothesis that the ",
-    "true population proportion is equal to ", test$null.value, ". Using a ",
-    "significance cut-off of ", alpha, ", we "
-  )
+  if (onesamp == TRUE) {
+    stmt <- paste0(
+      "This was a one-sample proportion test of the null hypothesis that the ",
+      "true population proportion is equal to ", test$null.value, ". Using a ",
+      "significance cut-off of ", alpha, ", we "
+    )
+  }
+  
+  if (onesamp == FALSE) {
+    stmt <- paste0(
+      "This was a two-sample proportion test of the null hypothesis that the ",
+      "two population proportions are equal to each other. Using a ",
+      "significance cut-off of ", alpha, ", we "
+    )
+  }
   
   if (test$p.value < alpha) {
     stmt <- paste0(
-      stmt, "reject the null hypothesis, and conclude that the true population ",
-      "proportion is different than "
-    )
-  } else {
+      stmt, "reject the null hypothesis, and conclude that the ")
+    
+    if (onesamp == TRUE) {
+      stmt <- paste0(
+        stmt, "true population proportion is different than ", test$null.value) 
+    }
+    
+    if (onesamp == FALSE) {
+      stmt <- paste0(
+        stmt, "two population proportions are not equal") 
+    }
+    
+  } 
+  if (test$p.value >= alpha){
     stmt <- paste0(
-      stmt, "do not reject the null hypothesis, and cannot conclude that the ",
-      "true population proportion is different than "
+      stmt, "do not reject the null hypothesis, and cannot conclude that the "
     )
+    
+    if (onesamp == TRUE) {
+      stmt <- paste0(
+        stmt, "true population proportion is different than ", test$null.value
+      ) 
+    }
+    
+    if (onesamp == FALSE) {
+      stmt <- paste0(
+        stmt, "two population proportions are different from one another"
+      ) 
+    }
   }
 
-  stmt <- paste0(
-    stmt, test$null.value, ". The observed number of events is ",
-    prettyNum(unlist(strsplit(tmp$data.name, " "))[1], big.mark=","),
-    ", out of a total sample size of ",
-    prettyNum(unlist(strsplit(unlist(strsplit(tmp$data.name, ","))[1], " "))[4], big.mark=","),
-    '.'
-  )
+  if (onesamp == TRUE) {
+    
+    samps <- strsplit(test$data.name, ",") %>%
+      unlist() %>%
+      strsplit(split = " ") %>%
+      unlist()
+    
+    x <- as.numeric(samps[1])
+    n <- as.numeric(samps[4])
+        
+    stmt <- paste0(
+      stmt, ". The observed number of events is ",
+      prettyNum(x, big.mark=","),
+      ", out of a total sample size of ",
+      prettyNum(n, big.mark=","),
+      '.'
+    )    
+  }
+  
+  if (onesamp == FALSE) {
+
+    samps <- strsplit(test$data.name, "\\(" ) %>%
+      unlist() %>%
+      strsplit(split="\\)") %>%
+      unlist() %>%
+      strsplit(split=",") %>%
+      unlist()
+    
+    x1 <- as.numeric(samps[2])
+    x2 <- as.numeric(samps[3])
+    n1 <- as.numeric(samps[5])
+    n2 <- as.numeric(samps[6])    
+    
+    stmt <- paste0(
+      stmt, ". The observed number of events in the first group is ",
+      prettyNum(x1, big.mark=","), ", out of a total sample size of ",
+      prettyNum(n1, big.mark=","), ". For the second group, the observed number of events is ",
+      prettyNum(x2, big.mark=","), ", out of a total sample size of ",
+      prettyNum(n2, big.mark=","), "."
+    )
+    
+    
+  }
+
+
+  
+#### need to two-samp-ify below here
   
   stmt <- paste0(
     stmt, "\n\nThe confidence interval for the true population proportion is (",
     test$conf.int[1], ', ', test$conf.int[2], '). That is, 95 times out of 100, ',
     "this interval will contain the true population proportion."
   )
-
-################################################################################  
+  ################################################################################
   stmt <- paste0(
     stmt, '\n\nThe p-value for this test is ', test$p.value, '. This, formally',
     ', is defined as the probability of observing a sample proportion that is ',
-    'as or more extreme than the observed sample proportion, assuming that the',
+    'as or more extreme than the observed sample proportion, assuming that the ',
     'null hypothesis is true. In this case, this is the probability of ',
     'observing a sample proportion that is greater than ',
     test$null.value + abs(test$estimate - test$null.value), ' or less than ',
     test$null.value - abs(test$estimate - test$null.value), ' asumming the null',
     'hypothesis (that the population proportion is ', test$null.value, ') were ',
-    'really true. A smaller p-value can be used to reject the null hypothesis, ',
+    'really true. A smaller p-value can be used to reject the null hypothesis, ',    
     'and conclude that the data are not consistent with a true population ',
     'proportion of ', test$null.value, '. A common cutoff convention is a ',
     'p-value of less than 0.05.'
@@ -58,9 +130,8 @@ summary_text <- function(test, alpha=0.05){
   
   return(cat(stmt))
   
-  # figure out how to make [1] go away.
-  # todo: flesh out for one sample prop test
-  # then add two sample prop test
+  # todo: 
+  # add two sample prop test
   # then do one sample t-test
   # then two-sample t-text
   # will need to distinguish between these (maybe by header?)
--- eda.R
+++ eda.R
@@ -8,8 +8,20 @@ install('textualize')
 
 setwd("~/Desktop/textualize/")
 
-
+# one samp, do not reject
 tmp <- prop.test(x=500, n=1008, correct=FALSE)
+summary_text(tmp)
+
+# one samp, reject
+tmp <- prop.test(x=100, n=1008, correct=FALSE)
+summary_text(tmp)
+
+# two samps, do not reject
+tmp <- prop.test(x=c(500,501), n=c(1008,1002), correct=FALSE)
+summary_text(tmp)
+
+# two samps, reject
+tmp <- prop.test(x=c(500,600), n=c(1008,1050), correct=FALSE)
 summary_text(tmp)
 
 
